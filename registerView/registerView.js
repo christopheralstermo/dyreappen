fd229d7baabb7020081ae0a5aacc22937bc2150d
Mille-booop <mille.kristiansen@hotmail.no>
2025-03-26 12:13:31 +0100

    css auth

--- registerView/registerView.js
+++ registerView/registerView.js
@@ -3,22 +3,22 @@ function registerView() {
     <h2>Registrer deg</h2>
     <form onsubmit="handleRegister(event)">
     <label for="name">Fornavn:</label>
-    <input type="text" id="name" name="name" oninput="model.inputs.register.name = this.value">;
+    <input type="text" id="name" name="name" oninput="model.inputs.register.name = this.value">
     <br>
     <label for="lastName">Etternavn:</label>
-    <input type="text" id="lastName" name="lastName" oninput="model.inputs.register.lastName = this.value">;
+    <input type="text" id="lastName" name="lastName" oninput="model.inputs.register.lastName = this.value">
     <br>
     <label for="email">Epost:</label>
-    <input type="email" id="email" name="email" oninput="model.inputs.register.email = this.value">;
+    <input type="email" id="email" name="email" oninput="model.inputs.register.email = this.value">
     <br>
     <label for="username">Brukernavn:</label>
-    <input type="text" id="username" name="username" oninput="model.inputs.register.username = this.value">;
+    <input type="text" id="username" name="username" oninput="model.inputs.register.username = this.value">
     <br>
     <label for="password">Passord:</label>
-    <input type="password" id="password" name="password" oninput="model.inputs.register.password = this.value">;
+    <input type="password" id="password" name="password" oninput="model.inputs.register.password = this.value">
     <br>
     <label for="repeatPassword">Gjenta passord:</label>
-    <input type="password" id="repeatPassword" name="repeatPassword" oninput="model.inputs.register.repeatPassword = this.value">;
+    <input type="password" id="repeatPassword" name="repeatPassword" oninput="model.inputs.register.repeatPassword = this.value">
     <br>
     <button type="submit">Registrer deg</button>
     </form>
