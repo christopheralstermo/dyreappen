c9029f242251ed7f64c68a36aed605ae24746954
Mille-booop <mille.kristiansen@hotmail.no>
2025-03-26 12:28:50 +0100

    fuckup fixed

--- loginView/loginView.js
+++ loginView/loginView.js
@@ -3,10 +3,10 @@ function logInView() {
     <h2>Logg inn</h2>
     <form onsubmit="handleLogin(event)">
     <label for="username">Brukernavn:</label>
-    <input type="text" id="username" name="username" oninput="model.inputs.loginForm.username = this.value">;
+    <input type="text" id="username" name="username" oninput="model.inputs.loginForm.username = this.value">
     <br>
     <label for="password">Passord:</label>
-    <input type="password" id="password" name="password" oninput="model.inputs.loginForm.password = this.value">;>
+    <input type="password" id="password" name="password" oninput="model.inputs.loginForm.password = this.value">
     <br>
     <button type="submit">Logg inn</button>
     </form>
