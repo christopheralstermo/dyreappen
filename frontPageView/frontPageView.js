62cabe881f77979a3f66779cf607b6972e80c834
Christopher Alstermo <christopher_alstermo@hotmail.com>
2025-03-27 08:38:00 +0100

    min siste update for uka

--- frontPageView/frontPageView.js
+++ frontPageView/frontPageView.js
@@ -2,60 +2,28 @@ function frontPageView() {
     return `
     <div id="frontPage">
         <div class="filter">
-        ${animalHtml()}
-        <img src="frontPageView/theMap.png">
-        ${areaHtml()}
+            ${animalHtml()}
+            <img id="gpsBildet" src="frontPageView/theMap.png">
+            ${areaHtml()}
         </div>
         ${animalProfileHtml()}
     </div>
     `;
 }
 
-
-function animalProfileHtml() {
-    let html = /*html*/`
-    <div class="profiles">
-    `; 
-    let index = 0;
-
-    for (let theAnimal of model.data.animals) {
-        let dyrenavn = theAnimal.name;
-        let art = theAnimal.animal;
-        let bildet = theAnimal.picture;
-        html += /*html*/`
-        
-        <div class="profile-card">
-            <h3 id="ask">Be om å leke med ${dyrenavn}</h3>
-            <div class="profile-image" style="width: 100px; height: 180px;"><img src="${bildet}"></div>
-            <div class="profile-details">
-                <p>${dyrenavn} - ${art}</p>
-                <p>4</p>
-                <p>oslo</p>
-                <p>vil møte: hund, katt</p>
-            </div>
-            <button>vis profil</button>
-            <div class="stars" data-rating="5" onclick="updateRating(${index})">
-                <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
-            </div>
-        </div>
-            `;
-        index++;
-    }
-    html += '</div>';
-
-
-    return html;
-}
-
-
 function animalHtml() {
-    let html = /*html*/`
-        <h3>Dyr</h3>
-    `;
-    for(animals of model.data.categories.animals){
-       html += `<label><input type="checkbox">${animals}</label>`
+    let html = /*html*/`<h3>Dyr</h3>`;
+    for (let animal of model.data.categories.animals) {
+        html += /*html*/`
+        <label>
+            <input 
+                type="checkbox"
+                onchange="updateFilter('${animal}', this.checked)"
+                ${model.inputs.filters.selectedAnimals.includes(animal) ? 'checked' : ''}
+            >${animal}</label>
+        `;
     }
-    return html
+    return html;
 }
 
 function areaHtml() {
@@ -67,3 +35,49 @@ function areaHtml() {
     }
     return html
 }
+
+function animalProfileHtml() {
+    let html = /*html*/`<div class="profiles">`;
+    let index = 0;
+
+    let filteredAnimals = model.data.animals.filter(animal => {
+        return model.inputs.filters.selectedAnimals.length === 0 || 
+               model.inputs.filters.selectedAnimals.includes(animal.animal);
+    });
+
+    for (let theAnimal of filteredAnimals) {
+        let dyrenavn = theAnimal.name;
+        let art = theAnimal.animal;
+        let bildet = theAnimal.picture;
+        html += /*html*/`
+        <div class="profile-card">
+            <h3 id="ask">Be om å leke med ${dyrenavn}</h3>
+            <div class="profile-image" style="height: 190px;"><img id="profilBildet" src="${bildet}"></div>
+            <div class="profile-details">
+                <p>${dyrenavn} - ${art}</p>
+                <p>${theAnimal.age}</p>
+                <p>${theAnimal.location}</p>
+                <p>vil møte: hund, katt</p>
+            </div>
+            <button id="profileBtn">vis profil</button>
+            <div class="stars" data-rating="5" onclick="updateRating(${index})">
+                <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+            </div>
+        </div>
+        `;
+        index++;
+    }
+    html += '</div>';
+    return html;
+}
+
+function updateFilter(animal, isChecked) {
+    if (isChecked) {
+        model.inputs.filters.selectedAnimals.push(animal);
+    } else {
+        model.inputs.filters.selectedAnimals = 
+            model.inputs.filters.selectedAnimals.filter(item => item !== animal);
+    }
+    updateView();
+}
+
